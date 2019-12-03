496ff32293316a392b300d2ce10624621a7a11b1
cijo <1005408974@qq.com>
2019-12-03 16:42:00 +0800

    origin

---
+++ news-user/src/main/java/com/yjls/NewsUserApplication.java
@@ -0,0 +1,13 @@
+package com.yjls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsUserApplication.class, args);
+	}
+
+}
---
+++ news-web/src/main/java/com/yjls/NewsWebApplication.java
@@ -0,0 +1,13 @@
+package com.yjls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsWebApplication.class, args);
+	}
+
+}
